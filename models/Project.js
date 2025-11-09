26a23501e83deb34bbedc86c564df6159b7a8e49
Muhammad Usman <usmansyedislamian@gmail.com>
2025-11-09 16:38:15 +0500

    finals

--- models/Project.js
+++ models/Project.js
@@ -1,39 +1,18 @@
-// server/models/Project.js
 const mongoose = require('mongoose');
 
 const ProjectSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-    trim: true
   },
   description: {
     type: String,
     required: true,
-    trim: true
   },
   technologies: [String],
-  image: {
-    type: String,
-    default: ''
-  },
-  githubLink: {
-    type: String,
-    default: ''
-  },
-  liveLink: {
-    type: String,
-    default: ''
-  },
-  category: {
-    type: String,
-    enum: ['web', 'mobile', 'fullstack', 'other'],
-    default: 'web'
-  },
-  featured: {
-    type: Boolean,
-    default: false
-  },
+  image: String,
+  githubLink: String,
+  liveLink: String,
   createdAt: {
     type: Date,
     default: Date.now,
